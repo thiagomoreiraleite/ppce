8d66765ba2d05e3b457c4d46ee415f5fedd37bbb
thiago moreira leite <thiagomoreiraleite@gmail.com>
2020-10-22 22:08:41 +0200

    code ongoing

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -11,11 +11,22 @@ const buildMap = () => {
   });
 };
 
+// const mapMarkers = []
+
 const addMarkersToMap = (map, markers) => {
   markers.forEach((marker) => {
-    new mapboxgl.Marker()
+    const newMarker = new mapboxgl.Marker()
       .setLngLat([ marker.lng, marker.lat ])
       .addTo(map);
+    // mapMarker.push(newMarker)
+    // We use the "getElement" funtion provided by mapbox-gl to access to the marker's HTML an set an id
+    newMarker.getElement().dataset.markerId = marker.cidade;
+    newMarker.getElement().dataset.markerCargo = marker.cargo;
+    // Put a microphone on the new marker listening for a mouseenter event
+    newMarker.getElement().addEventListener('click', (e) => updateSidebar(e) );
+    newMarker.getElement().addEventListener('mouseenter', (e) => changeCursorStyle(e) );
+    // We put a microphone on listening for a mouseleave event
+    // newMarker.getElement().addEventListener('mouseleave', (e) => updateSidebar(e) );
   });
 };
 
@@ -35,5 +46,27 @@ const initMapbox = () => {
       mapboxgl: mapboxgl }));
   }
 };
+
+const changeCursorStyle = (event) => {
+  event.currentTarget.style.cursor = 'pointer';
+}
+
+const updateSidebar = (event) => {
+  const cidadeInput = document.getElementById("cidade");
+
+  cidadeInput.innerText = `${event.currentTarget.dataset.markerId}`;
+  // event.currentTarget.dataset.markerId.forEach((marker) => {
+
+  // });
+}
+
+// const toggleCardHighlighting = (event) => {
+//   // We select the card corresponding to the marker's id
+//   const card = document.querySelector(`[data-flat-id="${event.currentTarget.dataset.markerId}"]`);
+//   // Then we toggle the class "highlight github" to the card
+//   card.classList.toggle('highlight');
+// }
+
+
 
 export { initMapbox };
