24917ab3f7a621822c1e0db1d95ce4d52d8ca92d
thiago moreira leite <thiagomoreiraleite@gmail.com>
2020-11-12 22:26:02 +0100

    code ongoing

--- app/javascript/packs/navbar.js
+++ app/javascript/packs/navbar.js
@@ -1,9 +1,45 @@
-function navbar() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-92px";
-  }
-}
+const navbar = () => {
+  window.onscroll = function() {scrollFunction() && myFunction()};
 
+  // Get the navbar
+   // Get the offset position of the conteudoSec
+  const conteudoSec = document.querySelector("div.conteudo-sec");
+
+ 
+  const sticky = conteudoSec.offsetTop;
+
+  // console.log(sticky);
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("navbar").style.top = "0";
+      if (window.pageYOffset >= sticky) {
+        // conteudoSec.classList.add("sticky");
+        conteudoSec.className = "sticky";
+      } else {
+        // conteudoSec.classList.remove("sticky");
+        conteudoSec.className = "conteudo-sec";
+      }
+    } else {
+      document.getElementById("navbar").style.top = "-75px";
+    }
+  }
+
+  // window.onscroll = function() {myFunction()};
+  // Get the navbar
+  // const conteudoSec = document.querySelector("div.conteudo-sec");
+
+  // // Get the offset position of the conteudoSec
+  // const sticky = conteudoSec.offsetTop;
+
+  // function myFunction() {
+  //   if (window.pageYOffset >= sticky) {
+  //     // conteudoSec.classList.add("sticky");
+  //     conteudoSec.className = "sticky";
+  //   } else {
+  //     // conteudoSec.classList.remove("sticky");
+  //     conteudoSec.className = "conteudo-sec";
+  //   }
+  // }
+}
 export { navbar };
