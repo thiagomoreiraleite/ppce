1dae318482d6a384488d2ed3c24f3983ec8b8d40
thiago leite <thiagomoreiraleite@gmail.com>
2021-01-26 22:43:28 -0300

    code changed

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -33,11 +33,11 @@ const addMarkersToMap = (map, markers) => {
   });
 };
 
-// const fitMapToMarkers = (map, markers) => {
-//   const bounds = new mapboxgl.LngLatBounds();
-//   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-//   map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
-// };
+const fitMapToMarkers = (map, markers) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
+};
 
 const initMapbox = () => {
   if (mapElement) {
